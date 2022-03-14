96945e9c63e6373c1522a967b3ca065ed187942b
nad0410 <96126078+nad0410@users.noreply.github.com>
2022-03-14 19:32:05 +0100

    fin de journee

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,18 +2,131 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+/**
+ * @Route("/product", name="app_catalog_")
+ * 
+ * site.com/products            => Affiche la liste des produits
+ * site.com/product             => Creer un produit
+ * site.com/product/{id}        => Afficher le detail d'un produit
+ * site.com/product/{id}/edit   => Modifier un produit
+ * site.com/product/{id}/delete => Supprime un produit
+ */
 class CatalogController extends AbstractController
 {
     /**
-     * @Route("/catalog", name="app_catalog_index")
+     * Affiche la liste des produits
+     * 
+     * url ex: /products
+     * name: app_catalog_index
+     * 
+     * @Route("s", name="index")
      */
     public function index(): Response
     {
         return $this->render('catalog/index.html.twig', [
         ]);
+    }
+
+    /**
+     * Ajouter un produit dans la bdd
+     * 
+     * url ex: /product
+     * name: app_catalog_create
+     * 
+     * @Route("", name="create")
+     */
+    public function create(Request $request, ValidatorInterface $validator): Response
+    {
+        $errors = [];
+
+        // Create Form
+        // --
+
+        // Initialiser le produit
+        $product = new Product;
+
+        
+        // Construction du formulaire
+        $form = $this->createForm(ProductType::class, $product);
+
+        // Handle the request
+        $form->handleRequest($request);
+
+        // Catch form submission
+        if ( $form->isSubmitted() ) 
+        {
+            // Handle form errors
+            $errors = $validator->validate( $product );
+
+            // si le form est valid
+            if ( $form->isValid() )
+            {
+                // dd( $product );
+
+                // enregistrement en bdd
+                $en = $this->getDoctrine()->getManager();
+                $en->persist( $product );
+                $en->flush();
+
+                // rediriger utilisateur
+                return $this->redirectToRoute('app_catalog_index');
+            }
+        }
+
+        // Préparation du formulaire pour la vue
+        $form = $form->createView();
+
+        // Response HTTP
+        // --
+        return $this->render('catalog/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * Afficher le detail d'un produit
+     * 
+     * url ex: /product
+     * name: app_catalog_read
+     * 
+     * @Route("/{id}", name="read")
+     */
+    public function read(int $id): Response
+    {
+
+    }
+
+    /**
+     * Modifier la fiche d'un produit
+     * 
+     * url ex: /product
+     * name: app_catalog_update
+     * 
+     * @Route("/{id}/edit", name="update")
+     */
+    public function update(int $id): Response
+    {
+        
+    }
+
+    /**
+     * Supprimer la fiche d'un produit
+     * 
+     * url ex: /product
+     * name: app_catalog_delete
+     * 
+     * @Route("/{id}/delete", name="delete")
+     */
+    public function delete(int $id): Response
+    {
+
     }
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=7, options={"fixed": true})
+     */
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
