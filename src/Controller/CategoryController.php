84225d876d456c33e02e20a00b414abb561143a2
nad0410 <96126078+nad0410@users.noreply.github.com>
2022-03-17 12:00:57 +0100

    jeudi commit1

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -20,7 +20,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("ies", name="index")
+     * @Route("ies", name="index", methods={"HEAD", "GET"})
      */
     public function index(CategoryRepository $categoryRepository): Response
     {
@@ -38,7 +38,7 @@ class CategoryController extends AbstractController
      * url ex: /category
      * name: app_category_create
      * 
-     * @Route("y", name="create")
+     * @Route("y", name="create", methods={"HEAD", "GET", "POST"})
      */
     public function create(ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator): Response
     {
@@ -107,7 +107,7 @@ class CategoryController extends AbstractController
      * url ex: /category
      * name: app_category_read
      * 
-     * @Route("y/{id}", name="read")
+     * @Route("y/{id}", name="read", methods={"HEAD", "GET"})
      */
     public function read(Category $category): Response
     {
@@ -124,12 +124,56 @@ class CategoryController extends AbstractController
      * url ex: /category
      * name: app_category_update
      * 
-     * @Route("y/{id}/edit", name="update")
+     * @Route("y/{id}/edit", name="update", methods={"HEAD", "GET", "POST"})
      */
-    public function update(int $id): Response
+    public function update(Category $category, ManagerRegistry $doctrine, Request $request, ValidatorInterface $validator): Response
     {
-        
+        // Construction du formulaire
+        $form = $this->createForm(CategoryType::class, $category);
+    
+        // Récupération de la requete HTTP
+        $form->handleRequest($request);
+    
+        // Test la soumission du formulaire
+        if ($form->isSubmitted())
+        {
+            // Récupération du CSRF Token
+            $csrf_token = $request->request->get( $form->getName() )['_csrf_category_token'];
+    
+            // Vérifier l'intégrité du formulaire (CSRF Token)
+            if ( ! $this->isCsrfTokenValid('_csrf_category_token_id', $csrf_token) )
+            {
+                    throw new \Exception("Erreur de token");
+            }
+                
+            // Validation du formulaire
+            $validator->validate( $category );
+    
+            if ($form->isValid())
+            {
+                // Ajouter des valeurs par défaut
+    
+                // Enregistrement des données en BDD
+                $em = $doctrine->getManager();
+                $em->persist( $category );
+                $em->flush();
+    
+                // Redirection de l'utilisateur vers la page du détail de la catégorie
+                return $this->redirectToRoute('app_category_read', [
+                    'id' => $category->getId()
+                ]);
+            }
+        }
+    
+        // Préparation du formulaire pour la vue
+        $form = $form->createView();
+    
+        // Transmission du formulaire à la vue
+        return $this->render('category/update.html.twig', [
+            'form' => $form
+        ]);
     }
+    
 
     /**
      * Supprimer la fiche d'une categorie
@@ -137,10 +181,34 @@ class CategoryController extends AbstractController
      * url ex: /category
      * name: app_category_delete
      * 
-     * @Route("y/{id}/delete", name="delete")
+     * @Route("y/{id}/delete", name="delete", methods={"HEAD", "GET", "DELETE"})
      */
-    public function delete(int $id): Response
+    public function delete(ManagerRegistry $doctrine, Category $category, Request $request): Response
     {
+        // condition
+        if ($request->getMethod() == 'DELETE')
+        {
+            // generer le message de confirmation de suppression
+            $message = "La categorie <strong>". $category->getName() ."</strong> à été supprimé.";
+
+            // supprimer de la BDD
+            $en = $doctrine->getManager();
+            $en->remove( $category );
+            $en->flush();
+
+            // creation d'un message
+            $this->addFlash('success', $message);
+
+            // redirection de l'utilisateur
+            return $this->redirectToRoute('app_category_index');
+
+         
+        }
+
+       // afficher message de confirmation
+       return $this->render('category/delete.html.twig', [
+           'category' => $category
+       ]); 
 
     }
 
